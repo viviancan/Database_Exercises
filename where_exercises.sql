f95353ad31a8721ac6a1968278c3af39a39b97fc
Vivian <vcnales@gmail.com>
2017-08-08 14:37:27 -0500

    added OR condition to 1st query

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,23 +1,32 @@
-/* SELECT *
-FROM employees
-WHERE first_name IN ("Irena" , "Vidya" , "Maya"); */
-
-/* SELECT *
-FROM employees
-WHERE last_name like "E%"; */
-
-/* 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' ; */
+WHERE first_name IN ("Irena" , "Vidya" , "Maya");
 
-/* SELECT *
+SELECT *
 FROM employees
-WHERE birth_date like '%-12-25'; */
+WHERE last_name like "E%";
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' ;
+
+SELECT *
+FROM employees
+WHERE birth_date like '%-12-25';
 
 SELECT *
 FROM employees
 WHERE last_name like '%q%';
+
+
+SELECT *
+FROM employees
+WHERE first_name = "Irena"
+	OR first_name = "Vidya"
+	OR first_name = "Maya";
+
+
+
 
 
 
