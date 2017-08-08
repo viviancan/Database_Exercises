5c36f8d864c9723b0e8f1b06b1120a2cbf9f1c5f
Vivian <vcnales@gmail.com>
2017-08-08 14:04:51 -0500

    Query for employees whose last name starts with E

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,7 @@
+/* SELECT *
+FROM employees
+WHERE first_name IN ("Irena" , "Vidya" , "Maya"); */
+
 SELECT *
 FROM employees
-WHERE first_name IN ("Irena" , "Vidya" , "Maya");
+WHERE last_name like "E%";
