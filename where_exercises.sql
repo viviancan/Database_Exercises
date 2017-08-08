d62ba0d6319f098b28aa5dec852fbb20491ebfbb
Vivian <vcnales@gmail.com>
2017-08-08 14:09:42 -0500

    Query for employees hired in the 90s

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,6 +2,10 @@
 FROM employees
 WHERE first_name IN ("Irena" , "Vidya" , "Maya"); */
 
+/* SELECT *
+FROM employees
+WHERE last_name like "E%"; */
+
 SELECT *
 FROM employees
-WHERE last_name like "E%";
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' ;
