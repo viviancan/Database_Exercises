bcc89cff19b9f6bf6589640673b2862eb73f4b57
Vivian <vcnales@gmail.com>
2017-08-08 14:11:11 -0500

    query for employees born on christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,6 +6,14 @@ WHERE first_name IN ("Irena" , "Vidya" , "Maya"); */
 FROM employees
 WHERE last_name like "E%"; */
 
+/* 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' ;
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' ; */
+
+SELECT *
+FROM employees
+WHERE birth_date like '%-12-25';
+
+
+
