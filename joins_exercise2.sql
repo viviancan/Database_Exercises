0bbcc1a3784411c4bf4d157bdc8a2ffc7477fab4
Vivian <vcnales@gmail.com>
2017-08-09 15:40:31 -0500

    found all deparments currenly managed by women

--- joins_exercise2.sql
+++ joins_exercise2.sql
@@ -6,3 +6,15 @@ JOIN dept_manager AS dm
 JOIN employees as e
 	ON e.emp_no = dm.emp_no
 WHERE dm.to_date like '9999-%';
+
+-- Find the name of all departments currently managed by women.
+SELECT  dep.dept_name AS 'Deparment Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Department Manager', e.gender
+FROM departments AS dep
+JOIN dept_manager AS dm
+	ON dep.dept_no = dm.dept_no
+JOIN employees as e
+	ON e.emp_no = dm.emp_no
+WHERE dm.to_date like '9999-%'
+AND e.gender = 'F';
+
+
