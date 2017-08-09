50cb1f67fb67b0280e1a2fb0166a6dfa0add7e6e
Vivian <vcnales@gmail.com>
2017-08-09 10:12:26 -0500

    soutions after walkthrough

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,2 +1,31 @@
+-- In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
 SELECT DISTINCT title
 FROM titles;
+
+-- Update the previous query to sort the results alphabetically.
+SELECT DISTINCT title
+FROM titles
+ORDER BY  title asc;
+
+-- Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
+SELECT  last_name 
+FROM employees
+WHERE last_name like "e%e"
+GROUP BY last_name;
+
+-- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows
+
+SELECT  first_name, last_name 
+FROM employees
+WHERE last_name like "e%e"
+GROUP BY last_name, first_name;
+
+-- Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. 
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name like "%q%"	
+	AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+
