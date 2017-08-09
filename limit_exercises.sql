e373351e30b0603e85b1701fafe92d2d8bdde5e7
Vivian <vcnales@gmail.com>
2017-08-09 10:34:44 -0500

    final commit b4 walkthrough

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -15,3 +15,11 @@ ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
 
 
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
+AND birth_date like '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
+;
+
