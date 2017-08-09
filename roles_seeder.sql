3001ed87437415219dc2a8bd7a848c9dd6632a25
Vivian <vcnales@gmail.com>
2017-08-09 15:18:11 -0500

    initial commit for join exercises

---
+++ roles_seeder.sql
@@ -0,0 +1,19 @@
+USE join_test_db;
+
+-- TRUNCATE roles; 
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+
+-- TRUNCATE users;
+
+INSERT INTO users (name, email, role_id) VALUES
+('bob', 'bob@example.com', 1),
+('joe', 'joe@example.com', 2),
+('sally', 'sally@example.com', 3),
+('adam', 'adam@example.com', 3),
+('jane', 'jane@example.com', null),
+('mike', 'mike@example.com', null);
