8b9b866ff4ec756306458ad236b949e8fe45dab0
Vivian <vcnales@gmail.com>
2017-08-09 10:26:25 -0500

    employees hired in the 90s and born on xmas -> first 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,14 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+-- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
+AND birth_date like '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+
