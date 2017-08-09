d318c782202d6eba24ad9c5b14dc6b80c90708d8
Vivian <vcnales@gmail.com>
2017-08-09 15:38:51 -0500

    problem 1 completed joins exercise 2

---
+++ joins_exercise2.sql
@@ -0,0 +1,8 @@
+-- write a query that shows each department along with the name of the current manager for that department.
+SELECT  dep.dept_name AS 'Deparment Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments AS dep
+JOIN dept_manager AS dm
+	ON dep.dept_no = dm.dept_no
+JOIN employees as e
+	ON e.emp_no = dm.emp_no
+WHERE dm.to_date like '9999-%';
