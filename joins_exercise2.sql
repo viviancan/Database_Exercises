abcbb613e44e386166c3f5df4699c88a9010ca5b
Vivian <vcnales@gmail.com>
2017-08-09 16:58:17 -0500

    changed like to now();

--- joins_exercise2.sql
+++ joins_exercise2.sql
@@ -5,28 +5,28 @@ JOIN dept_manager AS dm
 	ON dep.dept_no = dm.dept_no
 JOIN employees as e
 	ON e.emp_no = dm.emp_no
-WHERE dm.to_date like '9999-%';
+WHERE dm.to_date > now();
 
 -- Find the name of all departments currently managed by women.
-SELECT  dep.dept_name AS 'Deparment Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Department Manager', e.gender
+SELECT  dep.dept_name AS 'Deparment Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Manager Name'
 FROM departments AS dep
 JOIN dept_manager AS dm
 	ON dep.dept_no = dm.dept_no
 JOIN employees as e
 	ON e.emp_no = dm.emp_no
-WHERE dm.to_date like '9999-%'
+WHERE dm.to_date > now()
 AND e.gender = 'F';
 
 
 -- Find the current titles of employees currently working in the Customer Service department.
-SELECT  t.title AS 'Title', count(t.title) AS 'Count'
+SELECT  t.title AS 'Title', count(*) AS 'Count'
 FROM employees AS e
 JOIN dept_emp AS de
 	ON de.emp_no = e.emp_no
 JOIN titles AS t
 	 on t.emp_no = de.emp_no
-WHERE t.to_date like '9999-%'
-	AND de.to_date like '9999-%'
+WHERE t.to_date > now()
+	AND de.to_date > now()
 	AND de.dept_no = 'D009'
 GROUP BY t.title;
 
@@ -35,6 +35,7 @@ GROUP BY t.title;
 
 SELECT d.dept_name AS 'Department Name', 	CONCAT(e.first_name, ' ', e.last_name) AS 'Name' , 	s.salary AS 'Salary'
 FROM departments as d
+
 JOIN dept_manager as dm
 	ON dm.dept_no = d.dept_no
 
@@ -44,8 +45,8 @@ JOIN employees as e
 JOIN salaries as s
 	ON s.emp_no = e.emp_no
 
-WHERE dm.to_date like '9999%'
-	AND s.to_date like '9999%';
+WHERE dm.to_date > now()
+	AND s.to_date >now();
 
 
 /*  Find the names of all current employees, their department name, and their current manager's name. */
