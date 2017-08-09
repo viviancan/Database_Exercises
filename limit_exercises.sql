592b9bd107bc2f3d50a2b05b049878219bcd6906
Vivian <vcnales@gmail.com>
2017-08-09 10:24:30 -0500

    list first 10 last names

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,2 +1,6 @@
 -- List the first 10 distinct last name sorted in descending order. Your result should look like this:
 
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
